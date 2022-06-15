11c2cdeb095c5bd450081281c2d9a4d010f634e2
Andra Winters <andra.cierra@gmail.com>
2022-06-14 22:11:02 -0400

    commit on 'master'

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
-// Select color input
+const click = document.getElementByID('sizePicker')
+const color = document.getElementByID('colorPicker')
+const grid = document.getElementByID('pixelCanvas')
+const height = documetn.getElementByID('inputHeight')
+const width = document.getElementByID('inputWidth')
+
+
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(){
+if(table.innerHTML !== ""){
+    table.innerHTML=""
+}
 
-function makeGrid() {
+    for(let i = 0; i <gridWidth; i++){
+        let gridRow = document.createElement('tr');
+    for (let j= 1;j<height; j++){
 
-// Your code goes here!
+        let gridCell = document.createElement('td');
+        gridRow.appendChild(gridCell);
+        gridCell.addEventListener('hover', function(e) {
+            const color = document.querySelector('.color-picker').value;
+            this.style.backgroundColor = color;
+    })
+    }
+    gridRow.append(gridCell)
+    table.append(gridRow)
+}
 
 }
+    
+    // submit button
+    click.addeventListener("submit", function(event){
+        event.preventDefault();
+        makeGrid();
+    })
